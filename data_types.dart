d5b0a1d983ee414528e2738d3a82faa68bc07d52
ickembooi <kevinkipkemboi3@gmail.com>
2024-03-05 23:38:04 +0300

    first commit

---
+++ data_types.dart
@@ -0,0 +1,27 @@
+import 'dart:collection';
+
+void main() {
+  int num1 =
+      20; //Declaration of a variable of datatype int it's used for whole numbers
+  print("$num1");
+  double num2 =
+      30.1235; // declaration of variable of datatype double. its used for decimal numbers
+  print("$num2");
+
+  String name =
+      "James"; // declaration of variable name of string datatype. Its used for characters
+  print("My name is: $name");
+
+  Map<String, String> capital_city = {
+    "Kenya": "Nairobi",
+    "Uganda": "Kampala"
+  }; // this creates a map its similar to a dictionary in python
+  print("$capital_city");
+
+  List<String> cars = [
+    "Toyota",
+    "Mitsubishi",
+    "Subaru"
+  ]; //creates a list of cars
+  print("$cars");
+}
